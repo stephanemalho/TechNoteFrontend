711b9c6439a6a304ad6f2b232acc46b258141fe1
stephane.malho <stepahen.malho@gmail.com>
2022-12-04 22:42:23 +0100

    ✨ F03 > notes > add note Form

--- client/src/features/users/NewUserForm.js
+++ client/src/features/users/NewUserForm.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { useAddNewUserMutation } from "./usersApiSlice";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
+
+import { useAddNewUserMutation } from "./usersApiSlice";
 import { ROLES } from "../../config/roles";
 
 const USER_REGEX = /^[A-z]{3,20}$/;
@@ -106,6 +107,7 @@ const NewUserForm = () => {
           id="password"
           name="password"
           type="password"
+          autoComplete="off"
           value={password}
           onChange={onPasswordChanged}
         />
